43380756a19133c3693b374c32cbcf065eb16ba8
Tyler Laceby <tylerbadgertutorials@gmail.com>
2025-12-01 23:37:47 -0700

    2025/02 Part 1/2 completed

--- 2025/02/main.go
+++ 2025/02/main.go
@@ -2,11 +2,79 @@ package main
 
 import (
 	_ "embed"
+	"fmt"
+	"strconv"
+	"strings"
 )
 
 //go:embed input.txt
 var input string
 
+type Range struct {
+	Low  int
+	High int
+}
+
 func main() {
-	//
+	var sum = 0
+	for _, rng := range createIDRanges() {
+		for i := rng.Low; i <= rng.High; i++ {
+			if isBadID(fmt.Sprint(i)) {
+				sum += i
+			}
+		}
+	}
+
+	fmt.Printf("Sum of bad ID's: %d\n", sum)
+}
+
+func isBadID(id string) bool {
+	for patternLen := 1; patternLen <= len(id)/2; patternLen++ {
+		// constraints:
+		//  - must be possible to achieve 2 repitions (or more)
+		//  - pattern must repeat for all substrings in string
+		// PLAN: iterate over all pattern lengths which satisfy the constraints above
+		// for each length construct the pattern and check that all substrings of that length are of the pattern?
+		//  If one fails, it's not a pattern, if all succeed, the ID is invalid
+
+		if len(id)%patternLen != 0 {
+			continue
+		}
+
+		pattern := id[:patternLen]
+		isRepeating := true
+		maxRepitions := len(id) / patternLen
+		if maxRepitions < 2 {
+			continue
+		}
+
+		for offset := patternLen; offset < len(id); offset += patternLen {
+			if id[offset:offset+patternLen] != pattern {
+				isRepeating = false
+				break
+			}
+		}
+
+		if isRepeating {
+			return true
+		}
+	}
+
+	return false
+}
+
+func createIDRanges() []Range {
+	ranges := make([]Range, 0)
+
+	for _, idRange := range strings.Split(input, ",") {
+		spread := strings.Split(idRange, "-")
+		low, _ := strconv.Atoi(spread[0])
+		high, _ := strconv.Atoi(spread[1])
+		ranges = append(ranges, Range{
+			Low:  low,
+			High: high,
+		})
+	}
+
+	return ranges
 }
